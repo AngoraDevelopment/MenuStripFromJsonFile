27ec039a96c4a45f6367466dc6fa99846b7380bf
AngoraDevelopment <smokeshadowedgardo123@gmail.com>
2021-10-26 03:14:54 +0100

    add

---
+++ test/test/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(438, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(93)))), ((int)(((byte)(94)))));
+            this.ClientSize = new System.Drawing.Size(438, 379);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Load MenuStrip From JsonFile";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ test/test/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            AngoraDynamics di = new AngoraDynamics();
+            //Cargando el menu dinamico desde un archivo json
+            di.Load("menu.json", menuStrip1);
+        }
+    }
+
+    public class AngoraDynamics
+    {
+        public void Load(string json, MenuStrip menu)
+        {
+            StreamReader reader = new StreamReader(json);
+            string jsonText = reader.ReadToEnd();
+            reader.Close();
+            JObject obj = JObject.Parse(jsonText);
+            getAllProperties(json, obj, menu);
+        }
+
+        private void getAllProperties(string jsonPath, JToken children, MenuStrip menu)
+        {
+            MenuStrip mainNode = menu;
+            mainNode.Name = Path.GetFileNameWithoutExtension(jsonPath);
+
+            foreach (JToken doc in children.Children())
+            {
+                var property = doc as JProperty;
+                if (property != null)
+                {
+                    //############################################################
+                    //######### OBTIENE UN OBJETO VACIO DEL DOCUMENTO ############
+                    //############################################################
+                    if (property.Value.ToString() == "Menu")
+                    {
+                        ToolStripMenuItem menuItem = new ToolStripMenuItem(property.Name);
+                        menuItem.Name = property.Name + "MenuItem";
+                        menuItem.Tag = property.Name;
+                        mainNode.Items.AddRange(new ToolStripMenuItem[] { menuItem });
+                    }
+
+                    //###################################################################
+                    //######### OBTIENE UN OBJETO CON DROPDOWN DEL DOCUMENTO ############
+                    //###################################################################
+                    if (property.Value.Type == JTokenType.Object)
+                    {
+                        //AÑADIENDO AL MENU STRIP
+                        ToolStripMenuItem menuItem = new ToolStripMenuItem(property.Name);
+                        menuItem.Name = property.Name + "MenuItem";
+                        menuItem.Tag = property.Name;
+
+                        //CARGANDO HIJOS
+                        foreach (JToken item in property.Value.Children())
+                        {
+                            var property2 = item as JProperty;
+
+                            if (property2.Value.Type == JTokenType.String)
+                            {
+                                //###############################################################################
+                                //###### CARGANDO UN OBJETO DENTRO DE OTRO USANDO VALORES DISTINTOS #############
+                                //###############################################################################
+                                if (property2.Value.ToString() == "Options")
+                                {
+                                    ToolStripMenuItem menuItem2 = new ToolStripMenuItem(property2.Name);
+                                    menuItem2.Name = property2.Name + "MenuItem";
+                                    menuItem2.Tag = property2.Name;
+
+                                    foreach (JToken item2 in property.Value.Children())
+                                    {
+                                        var property3 = item2 as JProperty;
+
+                                        if (property3.Name == "displayFile")
+                                        {
+                                            string url = property3.Value.ToString().Replace("Url:", "").Trim();
+                                            string[] lines = File.ReadAllLines(url);
+                                            foreach (string l in lines)
+                                            {
+                                                ToolStripMenuItem display = new ToolStripMenuItem(l);
+                                                display.Name = l + "MenuItem";
+                                                display.Tag = l;
+                                                menuItem2.DropDownItems.Add(display);
+                                            }
+                                        }
+
+                                        if (property3.Value.ToString() == "Display")
+                                        {
+                                            ToolStripMenuItem display = new ToolStripMenuItem(property3.Name);
+                                            display.Name = property3.Name + "MenuItem";
+                                            display.Tag = property3.Name;
+                                            menuItem2.DropDownItems.Add(display);
+                                        }
+                                    }
+                                    menuItem.DropDownItems.Add(menuItem2);
+                                }
+                            }
+                            //###################################################
+                            //####### CARGANDO OBJETOS BASICOS SI HIJOS #########
+                            //###################################################
+                            if (property2.Value.Type == JTokenType.String)
+                            {
+                                if (property2.Value.ToString() == "OnClick")
+                                {
+                                    ToolStripMenuItem onClick = new ToolStripMenuItem(property2.Name);
+                                    onClick.Name = property2.Name + "MenuItem";
+                                    onClick.Tag = property2.Name;
+
+                                    menuItem.DropDownItems.Add(onClick);
+                                }
+
+                                if (property2.Value.ToString() == "Separator")
+                                {
+                                    ToolStripSeparator sep = new ToolStripSeparator();
+                                    menuItem.DropDownItems.Add(sep);
+                                }
+
+                            }
+                        }
+                        //AGREGO EL OBJETO AL MENU STRIP
+                        mainNode.Items.AddRange(new ToolStripMenuItem[] { menuItem });
+                    }
+                }
+                getAllProperties(jsonPath, doc, menu);
+            }
+
+        }
+    }
+}
